a7473dd478eb0a2ea1f71cbdb12bc72a19261337
GowthamORA <gepparla@GEPPARLA-IN.in.oracle.com>
2020-01-10 00:11:57 +0530

    Second

--- src/test/java/selenium/grid/GridDemoTest.java
+++ src/test/java/selenium/grid/GridDemoTest.java
@@ -21,23 +21,36 @@ public class GridDemoTest {
 	WebDriver driver;
 
 	@BeforeMethod
-	public void beforeMethod() throws MalformedURLException {
-		DesiredCapabilities cap = new DesiredCapabilities();
-		cap.setBrowserName("chrome");
-		cap.setPlatform(Platform.WINDOWS);
+	public void beforeMethod() {
+		DesiredCapabilities chromeCap = new DesiredCapabilities();
+		DesiredCapabilities firefoxCap = new DesiredCapabilities();
+		chromeCap.setBrowserName("chrome");
+		chromeCap.setPlatform(Platform.WINDOWS);
+		
+		firefoxCap.setBrowserName("firefox");
+		firefoxCap.setPlatform(Platform.WINDOWS);
 
 		ChromeOptions options = new ChromeOptions();
-		options.merge(cap);
-		driver = new RemoteWebDriver(new URL(hubURL), options);
+		options.merge(chromeCap);
+		try {
+			driver = new RemoteWebDriver(new URL(hubURL), firefoxCap);
+			//driver = new RemoteWebDriver(new URL(hubURL), options);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 		driver.get("https:\\www.gmail.com");
+		
+		
 	}
 
 	@Test
 	public void test1() {
+		System.out.println(driver.getTitle());;
 	}
 
 	@AfterMethod
 	public void afterMethod() {
+		driver.quit();
 	}
 
 }
